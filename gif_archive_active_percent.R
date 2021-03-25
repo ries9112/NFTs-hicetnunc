e1a3d3ce1a86ac27f38f148e1c3888b295c85995
ries9112 <ries9112@colorado.edu>
2021-03-25 11:25:07 -0600

    added script to use archive to create GIF

---
+++ gif_archive_active_percent.R
@@ -0,0 +1,45 @@
+# MAKE GIF of archive
+library(magick)
+dir_out <- file.path("archive/active_users_percent")
+## list file names and read in
+imgs <- list.files(dir_out, full.names = TRUE)
+img_list <- lapply(imgs, image_read)
+
+## join the images together
+img_joined <- image_join(img_list)
+
+## animate at 2 frames per second
+img_animated <- image_animate(img_joined, fps = 2)
+
+## view animated image
+img_animated
+
+## save to disk
+image_write(image = img_animated,
+            path = "active_users_percent.gif")
+
+
+
+# Now make gif for number of daily users
+dir_out <- file.path("archive/users")
+
+imgs <- list.files(dir_out, full.names = TRUE)
+# Only keep all-time users
+imgs <- imgs[grepl( "all_users", imgs, fixed = TRUE)]
+img_list <- lapply(imgs, image_read)
+
+## join the images together
+img_joined <- image_join(img_list)
+
+## animate at 2 frames per second
+img_animated <- image_animate(img_joined, fps = 2)
+
+## view animated image
+img_animated
+
+## save to disk
+image_write(image = img_animated,
+            path = "all_time_users.gif")
+
+
+
